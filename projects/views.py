24a4a61872efec9d052477e3a5125fd7a16f8798
Kevin Schans <kevinvdsd@hotmail.com>
2024-06-25 22:32:26 +0200

    Render projects list

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional e-commerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    message = 'Hello'
+
+    return render(request, 'projects/projects.html', {'message': message, 'projects': projectsList})
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
