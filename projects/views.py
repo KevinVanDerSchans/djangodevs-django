0c2b0683b99adca0ce0f0b1db8c92ca220084a59
Kevin Schans <kevinvdsd@hotmail.com>
2024-06-25 21:38:21 +0200

    Urls configuration

--- projects/views.py
+++ projects/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + pk)
