b02599975da6ab96150d34ec375f34580a8071a1
Kevin Schans <kevinvdsd@hotmail.com>
2024-07-03 01:07:07 +0200

    Add images confi in settings.py

--- djangodevs_django/settings.py
+++ djangodevs_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kf#eqmkm)8e-96f#nmjl13+bmiy%7uw$kg!#3x%wa02hkx9hu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
