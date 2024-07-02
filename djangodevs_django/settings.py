fc81536d5ab5d6b5a692ce065fe33b0dff5779d5
Kevin Schans <kevinvdsd@hotmail.com>
2024-07-03 01:11:30 +0200

    Set allowed hosts

--- djangodevs_django/settings.py
+++ djangodevs_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kf#eqmkm)8e-96f#nmjl13+bmiy%7uw$kg!#3x%wa02hkx9hu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
