fbe7f20ce9d59be7200e5b5c415476e5bc67e3aa
Kevin Schans <kevinvdsd@hotmail.com>
2024-07-06 22:00:26 +0200

    Create ProfileForm in forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -21,3 +22,21 @@ class CustomUserCreationForm(UserCreationForm):
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'name',
+            'username',
+            'location',
+            'email',
+            'short_intro',
+            'bio',
+            'profile_image',
+            'social_github',
+            'social_twitter',
+            'social_linkedin',
+            'social_youtube',
+            'social_website'
+        ]
