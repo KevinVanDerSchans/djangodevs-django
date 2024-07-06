a4bbda2a599b2335b1ae7c257443057e8a8bde30
Kevin Schans <kevinvdsd@hotmail.com>
2024-07-06 18:04:13 +0200

    Create login view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
+
+def loginPage(request):
+    if (request.method == 'POST'):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username does not exist in the database')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Username or password is incorrect')
+
+    return render(request, 'users/login_register.html')
 
 def profiles(request):
     profiles = Profile.objects.all()
